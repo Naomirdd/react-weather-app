3ebe12e63131e3e0d7ceb256b26a00bbb2908e05
Naomirdd <riddnaomi1@gmail.com>
2024-03-09 15:46:28 +0000

    added github url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1> Weather App </h1>
+      <footer>
+        The project is created by Naomi Riddington
+        <a href="https://app.netlify.com/start/repos?entryPoint=from%20TeamOverview">
+          {" "}
+          and open sourced on Github{" "}
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
